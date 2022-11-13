77facabca14933203286ddb242fbb7a4281d9171
canojuanpa800 <canojuanpa@gmail.com>
2022-11-13 16:04:12 -0500

    attempt 1

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,41 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
+import datetime as dt
 
+def campo_a_minuscula(df,campo):
+    df[campo] = df[campo].str.capitalize()
+    return df
+
+def borrar_char(df,campo):
+    df[campo] = df[campo].str.replace('_',' ')
+    df[campo] = df[campo].str.replace('-',' ')
+    return df
 
 def clean_data():
+    
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", encoding='utf8')
+    
+    # Tenga en cuenta datos faltantes
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
 
-    #
-    # Inserte su código aquí
-    #
-
+    df = campo_a_minuscula(df, 'sexo')
+    df = campo_a_minuscula(df, 'tipo_de_emprendimiento')
+    df = campo_a_minuscula(df, 'idea_negocio')
+    df = campo_a_minuscula(df, 'barrio')
+    df = campo_a_minuscula(df,'línea_credito')
+    df = borrar_char(df, 'idea_negocio')
+    df = borrar_char(df, 'barrio')
+    df = borrar_char(df,'línea_credito')
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('$',"",regex=False)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('.00',"",regex=False)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',',"")
+    df['monto_del_credito'] = df['monto_del_credito'].astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: dt.datetime.strptime(x, "%d/%m/%Y") if (x[-5] == '/' and x[-8] == '/') else dt.datetime.strptime(x, "%Y/%m/%d"))
+    
+    # Tenga en cuenta duplicados
+    df.drop_duplicates(inplace = True)
     return df
